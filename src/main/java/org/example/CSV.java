14f5f1a2720c361ddcb74329490b330bca0a5e5e
alvaro <alvarogarciagimeno@gmail.com>
2025-02-25 11:39:50 +0100

    Resources

--- src/main/java/org/example/CSV.java
+++ src/main/java/org/example/CSV.java
@@ -1,6 +1,12 @@
 package org.example;
 
+import java.net.URISyntaxException;
+
 public class CSV {
+    String fichero = getClass().getClassLoader().getResource("iris.csv").toURI().getPath();
+
+
+
     public Table readTable(String valor){
         return null;
     }
